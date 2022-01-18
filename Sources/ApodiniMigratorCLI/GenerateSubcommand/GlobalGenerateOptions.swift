cb5c2956000f66ab26ab55125d9ea3f6bfaf4f03
Andreas Bauer <andi.bauer@tum.de>
2022-01-18 02:29:07 +0100

    REUSE

--- Sources/ApodiniMigratorCLI/GenerateSubcommand/GlobalGenerateOptions.swift
+++ Sources/ApodiniMigratorCLI/GenerateSubcommand/GlobalGenerateOptions.swift
@@ -1,5 +1,9 @@
 //
-// Created by Andreas Bauer on 18.01.22.
+// This source file is part of the Apodini open source project
+//
+// SPDX-FileCopyrightText: 2019-2022 Paul Schmiedmayer and the Apodini project authors (see CONTRIBUTORS.md) <paul.schmiedmayer@tum.de>
+//
+// SPDX-License-Identifier: MIT
 //
 
 import Foundation
